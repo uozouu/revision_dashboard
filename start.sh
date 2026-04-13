eaa4a53bcbd768d504fe04ad783e94ccff1cd5e0
uozouu <ujjwalkumaroozoo@gmail.com>
2026-04-13 22:03:16 +0530

    Activate virtual environment before starting Gunicorn

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+source /app/.venv/bin/activate
 cd Backend
-python3.11 -m gunicorn wsgi:application --bind 0.0.0.0:$PORT --worker-class eventlet --workers 1 --timeout 120
+gunicorn wsgi:application --bind 0.0.0.0:$PORT --worker-class eventlet --workers 1 --timeout 120
